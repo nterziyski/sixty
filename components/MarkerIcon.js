b2add6d4ff897b29f40f03631dac8f7fa5d5d34b
Burger, Patrick <Patrick.Burger@sport1.de>
2018-11-17 18:27:52 +0100

    style MarkerIcons

--- components/MarkerIcon.js
+++ components/MarkerIcon.js
@@ -2,16 +2,22 @@ import React from 'react'
 import { TouchableOpacity, View, Text, Image } from 'react-native'
 
 export default class MarkerIcon extends React.PureComponent {
-    
     render() {
-        const styles = {
-            postContainer: null,
-            userInformations: null,
-            status: null
+        var styles = {
+            width: 40,
+            height: 40,
+            borderRadius: 5
+        }
+
+        if (this.props.hasFavoriteCars) {
+            styles.borderWidth = 3
+            styles.borderColor = '#FF5F00' // sixt orange
         }
 
         return (
-            <Image source={require('../assets/images/sixt_marker.png')} style={{width: 30, height: 30}}/>
-        )
-    }
+                <Image source={require('../assets/images/sixt_marker.png')} style={styles}/>
+            )
+        }
 }
+
+MarkerIcon.defaultProps = { hasFavoriteCars: false }
