84e2daa899fa10e5996ede6ec89e7b92bbf67ab3
Nikolay Terziyski <nikolay.terziyski@holidu.com>
2018-11-17 14:45:27 +0100

    Links screen => Map Screen. Add burger overlay

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -4,7 +4,7 @@ import { createStackNavigator, createBottomTabNavigator } from 'react-navigation
 
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
+import MapScreen from '../screens/MapScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 
 const HomeStack = createStackNavigator({
@@ -25,16 +25,16 @@ HomeStack.navigationOptions = {
   ),
 };
 
-const LinksStack = createStackNavigator({
-  Links: LinksScreen,
+const MapStack = createStackNavigator({
+  Map: MapScreen,
 });
 
-LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
+MapStack.navigationOptions = {
+  tabBarLabel: 'Map',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
+      name={Platform.OS === 'ios' ? 'ios-map' : 'md-map'}
     />
   ),
 };
@@ -55,6 +55,6 @@ SettingsStack.navigationOptions = {
 
 export default createBottomTabNavigator({
   HomeStack,
-  LinksStack,
+  MapStack,
   SettingsStack,
 });
---
+++ screens/MapScreen.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { MapView } from 'expo' 
+import { StyleSheet, View, Alert } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+export default class MapScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      region: {
+        latitude: 48.35397931,
+        longitude: 11.78138889,
+        latitudeDelta: 0.1122,
+        longitudeDelta: 0.1121,
+      }
+    }
+    }
+
+  static navigationOptions = {
+    title: 'Map',
+    header: null,
+  };
+  
+  onRegionChange = (region) => {
+    this.setState({ region });
+  }
+  
+  openMenu = () => {
+    Alert.alert('1')
+  }
+
+  render() {
+    const { viewStyles, iconContainer } = styles
+    return (
+      <View style={viewStyles}>
+        <View style={iconContainer} >
+          <Ionicons onPress={this.openMenu} name="md-menu" size={32} color="black" />
+        </View>
+        <MapView
+          style={{ flex: 1 }}
+          region={this.state.region}
+          onRegionChange={this.onRegionChange}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+  },
+  viewStyles: {
+    position: 'relative',
+    flex: 1
+  },
+  iconContainer: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+    zIndex: 10,
+    backgroundColor: 'rgba(255, 255, 255, 0.6)',
+    paddingHorizontal: 10,
+    borderRadius: 3,
+  }
+});
